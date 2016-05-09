52e6933c9df571b9a72e831f3e5250cad6bad076
Michael Berman <michael.berman@holler.com.au>
2016-05-09 14:42:28 +1000

    Basic data-based template compilation

---
+++ templa.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
+const mkpath = require('mkpath');
+const nunjucks = require('nunjucks');
+const yaml = require('js-yaml');
+const yamlFront = require('yaml-front-matter');
+
+
+function loadDataFile(filePath) {
+	const fileContents = fs.readFileSync(filePath);
+	const type = path.extname(filePath);
+
+	if (type === '.yaml') {
+		return yaml.safeLoad(fileContents);
+	} else if (type === '.json') {
+		return JSON.parse(fileContents);
+	}
+
+	const data = yamlFront.loadFront(fileContents, 'content');
+	if (type === '.md') {
+		data.content = marked(data.content);
+	}
+
+	return data;
+}
+
+function loadDataDir(dirPath) {
+	const dirContents = fs.readdirSync(dirPath);
+	const data = [];
+
+	dirContents.forEach((fileName) => {
+		const filePath = path.format({ dir: dirPath, base: fileName });
+		const fileData = loadDataFile(filePath);
+		if (!fileData.slug) {
+			fileData.slug = path.parse(fileName).name;
+		}
+		data.push(fileData);
+	});
+
+
+	// data.slug = data.slug || dataFile.replace(/\..*/, '');
+	return data;
+}
+
+function loadData(obj, baseDir) {
+	return obj.file ?
+		loadDataFile(path.format({ dir: baseDir, base: obj.file })) :
+		loadDataDir(path.format({ dir: baseDir, base: obj.dir }));
+}
+
+
+function templa(cb) {
+	const config = JSON.parse(fs.readFileSync('.templa.json'));
+	nunjucks.configure(config.baseTemplateDir);
+
+	// Get global data
+	const globalData = {};
+	if (config.globalData) {
+		config.globalData.forEach((obj) => {
+			const data = loadData(obj, config.baseDataDir);
+			if (obj.key) {
+				globalData[obj.key] = data;
+			} else {
+				Object.assign(globalData, data);
+			}
+		});
+	}
+
+
+	// Get page list
+	const pages = [];
+	config.pages.forEach((pageType) => {
+		if (!pageType.data) {
+			pages.push(pageType);
+		} else {
+			const dataItems = loadData(pageType.data, config.baseDataDir, true);
+			const { key } = pageType.data;
+
+			dataItems.forEach((data) => {
+				const url = pageType.url.replace(/:slug/g, data.slug);
+
+				pages.push(Object.assign({}, pageType, {
+					url,
+					data: Object.assign({ url }, key ? { [key]: data } : data),
+				}));
+			});
+		}
+	});
+
+	// Render pages
+	pages.forEach((page) => {
+		const fileName = `${page.url.replace(/\/$/, '/index').replace(/^\//, '')}.html`;
+		const filePath = `./tmp/${fileName}`;
+		const output = nunjucks.render(`${page.template}.html.nunjucks`, Object.assign(
+			{},
+			globalData,
+			page.data
+		));
+		mkpath.sync(path.dirname(filePath));
+		fs.writeFileSync(filePath, output);
+	});
+
+	if (typeof cb === 'function') cb();
+}
+
+module.exports = templa;
